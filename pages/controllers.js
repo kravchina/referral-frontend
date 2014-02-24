fdade66bec55d4ef42de5ebca36a8f6616015ada
Anton Mezerny <mezerny@gmail.com>
2014-02-24 18:04:57 +0200

    Added CORS support for cross-domain requests, refactorings

--- pages/controllers.js
+++ pages/controllers.js
@@ -1,22 +1,40 @@
 var dentalLinks = angular.module('dentalLinks', []);
 
+post_request = function(http, action, data, options, success_callback, error_callback){
+    http.post('http://localhost:3000' + action, data, options).success(success_callback).error(error_callback)
+};
+
+get_request = function (http, action, options, success_callback, error_callback){
+    http.get('http://localhost:3000' + action, options).success(success_callback).error(error_callback);
+}        ;
+
+dentalLinks.config(['$httpProvider', function($httpProvider) {
+    $httpProvider.defaults.useXDomain = true;
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+}
+]);
+
 dentalLinks.controller('LoginController', ['$scope', '$http',
     function ($scope, $http) {
 
         $scope.login = function (user) {
-            $http.post('/sign_in', {'user': {'email': user.email, 'password': user.password }}).success(function (data) {
+            post_request($http, '/sign_in', {'user': {'email': user.email, 'password': user.password }},{}, function (data) {
                 $scope.token = data.token;
                 $scope.email = user.email;
                 $scope.redirectUrl = '/practice_invite'
 
+            }, function(data){
+                $scope.error = 'error'
             });
         }
     }]);
 
 dentalLinks.controller('PracticeInvitationsController', ['$scope', '$http', function ($scope, $http) {
     $scope.invite_practice = function (practice) {
-        $http.post('/practice_invitations', {'practice': practice, email: $scope.email, authentication_token: $scope.token
-        }).success(function (data) {
+        post_request($http, '/practice_invitations', {'practice': practice, email: $scope.email, authentication_token: $scope.token
+        }, function (data) {
+            $scope.invite_practice_result = data;
+        }, function (data){
             $scope.invite_practice_result = data;
         });
     }
@@ -24,16 +42,16 @@ dentalLinks.controller('PracticeInvitationsController', ['$scope', '$http', func
 
 dentalLinks.controller('ReferralsController', ['$scope', '$http', function ($scope, $http) {
 
-    $http.get('/patients', {'params': {'email': $scope.email, 'authentication_token': $scope.token}}).success(function (data) {
+    get_request($http, '/patients', {'params': {'email': $scope.email, 'authentication_token': $scope.token}}, function (data) {
         $scope.patients = data;
     });
 
-    $http.get('/practices', {'params': {'email': $scope.email, 'authentication_token': $scope.token}}).success(function (data) {
+    get_request($http, '/practices', {'params': {'email': $scope.email, 'authentication_token': $scope.token}}, function (data) {
         $scope.practices = data;
     });
 
     $scope.create_referral = function (referral) {
-        $http.post('/referrals', {'referral': referral, email: $scope.email, authentication_token: $scope.token}).success(function (data) {
+        post_request($http, '/referrals', {'referral': referral, email: $scope.email, authentication_token: $scope.token}, function (data) {
             $scope.create_referral_result = data;
         });
 
@@ -46,11 +64,11 @@ dentalLinks.controller('UsersController', ['$scope', '$http', function ($scope, 
         {'mask':2, 'name': 'doctor'},
         {'mask':4, 'name':'aux'}
     ];
-    $http.get('/practices', {'params': {'email': $scope.email, 'authentication_token': $scope.token}}).success(function (data) {
+    get_request($http, '/practices', {'params': {'email': $scope.email, 'authentication_token': $scope.token}}, function (data) {
         $scope.practices = data;
     });
     $scope.create_user = function (user) {
-        $http.post('/sign_up', {'user': user, email: $scope.email, authentication_token: $scope.token}).success(function (data) {
+        post_request($http, '/sign_up', {'user': user, email: $scope.email, authentication_token: $scope.token}, function (data) {
             $scope.create_user_result = data;
         });
     }
