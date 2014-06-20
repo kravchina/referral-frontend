bca729ed02bb9fb9f7f9c31f1b6d5d0a47d7f85d
Alexsey Popov <alexsey.popov718@gmail.com>
2014-06-20 21:42:22 +0300

    Story: Time values in History page

--- js/controllers/historyController.js
+++ js/controllers/historyController.js
@@ -6,4 +6,5 @@ historyModule.controller('HistoryController', ['$scope', 'Auth', 'Referral', fun
         $scope.referrals = Referral.findByPractice({id: Auth.get().practice_id, start_date: start.toISOString(), end_date: end.toISOString()});
     };
     $scope.findReferrals( moment().subtract('days', 29), moment());
+    console.log($scope.referrals)
 }]);
