eaf7f6d3bf8c4e3e38d0454743f3d52e624a0918
Alexsey Popov <alexsey.popov718@gmail.com>
2014-06-24 22:29:05 +0300

    Story: clicking anywhere on a row in history table - should open view referral page

--- js/controllers/historyController.js
+++ js/controllers/historyController.js
@@ -1,10 +1,15 @@
 var historyModule = angular.module('history', ['ui.bootstrap']);
 
-historyModule.controller('HistoryController', ['$scope', 'Auth', 'Referral', function ($scope, Auth, Referral) {
+historyModule.controller('HistoryController', ['$scope', '$state', 'Auth', 'Referral', function ($scope, $state, Auth, Referral) {
 
     $scope.findReferrals = function (start, end) {
         $scope.referrals = Referral.findByPractice({id: Auth.get().practice_id, start_date: start.toISOString(), end_date: end.toISOString()});
     };
     $scope.findReferrals( moment().subtract('days', 29), moment());
     console.log($scope.referrals)
+
+    $scope.viewReferral = function(referral_id){
+    	console.log(referral_id);
+    	$state.go('viewReferral', {'referral_id': referral_id}, {reload: true});
+    }
 }]);
