cb8fe7aaecc9dee974cbdd88090093e5708c53da
Anton Mezerny <mezerny@gmail.com>
2014-06-27 15:51:27 +0300

    [#74017848] - Remove null and undefined text from download PDF and download Patient copy PDF

--- js/controllers/viewReferralsController.js
+++ js/controllers/viewReferralsController.js
@@ -30,7 +30,7 @@ viewReferralModule.controller('ViewReferralsController', ['$scope', '$stateParam
         $scope.referral.$promise.then(function (data) {
             PDF.addParagraph('Patient: ', data.patient.first_name + ' ' + data.patient.last_name);
             if (data.orig_provider) {
-                PDF.addParagraph('Original provider: ', data.orig_provider.first_name + ' ' + data.orig_provider.middle_initial + ' ' + data.orig_provider.last_name);
+                PDF.addParagraph('Original provider: ', data.orig_provider.first_name + ' ' + (data.orig_provider.middle_initial || '') + ' ' + (data.orig_provider.last_name || ''));
             }
             if ((data.dest_provider || {}).practice) {
                 PDF.addParagraph('Destination practice: ', (data.dest_provider.practice || {}).name);
@@ -42,10 +42,10 @@ viewReferralModule.controller('ViewReferralsController', ['$scope', '$stateParam
                 PDF.addParagraph('Practice website: ', address.website);
             }
             if (data.dest_provider) {
-                PDF.addParagraph('Destination provider: ', (data.dest_provider || {}).first_name + ' ' + (data.dest_provider || {}).middle_initial + ' ' + (data.dest_provider || {}).last_name);
+                PDF.addParagraph('Destination provider: ', (data.dest_provider || {}).first_name + ' ' + ((data.dest_provider || {}).middle_initial || '') + ' ' + ((data.dest_provider || {}).last_name || ''));
             }
             if (data.procedure) {
-                PDF.addParagraph('Procedure: ', data.procedure.name + '(' + (data.procedure.practice_type || {}).name + ')');
+                PDF.addParagraph('Procedure: ', data.procedure.name + ' (' + (data.procedure.practice_type || {}).name + ')');
             }
             if (data.teeth) {
                 data.teethChart = data.teeth.split('+');
