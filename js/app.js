18840710f9ae1fe2c91d43683467a04d5398d60b
Alexsey Popov <alexsey.popov718@gmail.com>
2014-08-18 11:42:53 +0300

    stripe configuration

--- js/app.js
+++ js/app.js
@@ -23,7 +23,7 @@ dentalLinks.constant('USER_ROLES', {
 });
 
 dentalLinks.constant('FREE_TRIAL_PERIOD', 45);
-dentalLinks.constant('STRIPE_KEY', 'pk_test_TAdWKoNc4HgjFknjuuzsb99p');
+dentalLinks.constant('STRIPE_KEY', 'pk_test_XUhHfTuR70aPg0tIf1bCYXqI');
 
 dentalLinks.constant('AUTH_EVENTS', {
     notAuthenticated: 'auth-not-authenticated'
