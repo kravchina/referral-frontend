3b29b1f056f0b7652353cff551ca0d0fdf1d6ddd
anton-kaidalov <anton.kaydalov@gmail.com>
2014-12-07 20:14:49 -0500

    [#82012974] - Changed page loading expectations

--- test/referral-frontend.js
+++ test/referral-frontend.js
@@ -1,7 +1,12 @@
+//var baseUrl = 'http://localhost:4000';
+var baseUrl = 'http://dev1.dentallinks.org';
+
 describe('when user logs in', function() {
     
+    browser.get(baseUrl + '/#/sign_in');
+    
     beforeEach(function() {
-        browser.get('http://dev1.dentallinks.org/#/sign_in');
+//        browser.get(baseUrl + '/#/sign_in');
         
         // check that account menu has class ng-hide, i.e. user is logged out
         expect(element(by.css('ul.nav.navbar-nav.navbar-right.ng-hide')).isPresent()).toBe(true);
@@ -12,19 +17,20 @@ describe('when user logs in', function() {
         element(by.buttonText('Login')).click();
     });
     
-    it('shows the top-right menu', function() {
+    it('lands on History page and shows the top-right menu', function() {
+        expect(browser.getCurrentUrl()).toContain('/history');
         expect(element(by.css('ul.nav.navbar-nav.navbar-right')).isPresent()).toBe(true);
-        browser.debugger();
-    });    
+    });
     
     describe('when user navigates to Admin', function() {
         beforeEach(function() {
-            browser.get('http://dev1.dentallinks.org/#/admin');
+            expect(browser.getCurrentUrl()).toContain('/history'); // TODO [ak] experiment. Remove this
+            expect(element(by.css('ul.nav.navbar-nav.navbar-right')).isPresent()).toBe(true);
+            browser.get(baseUrl + '/#/admin');
         });
         
         it('navigates to admin', function() {
-            expect(element(by.model('practice.account_first_name')).isPresent()).toBe(true);
-            browser.debugger();
+            expect(element(by.model('practice.account_first_name')).isPresent()).toBe(true); // find better (=more) criteria of recognizing Admin page
         });
         
     });
@@ -32,47 +38,33 @@ describe('when user logs in', function() {
     describe('when user navigates to Create Referral', function() {
         
         beforeEach(function() {
-            browser.get('http://dev1.dentallinks.org/#/create_referral');
+            expect(browser.getCurrentUrl()).toContain('/history'); // TODO [ak] experiment. Remove this
+            expect(element(by.css('ul.nav.navbar-nav.navbar-right')).isPresent()).toBe(true);
+            browser.get(baseUrl + '/#/create_referral');
         });
         
         it('shows Create Referral page', function() {
-            expect(element(by.model('patient')).isPresent()).toBe(true); // find better criteria of recognizing Create Referral page
-            browser.debugger();
+            expect(element(by.model('patient')).isPresent()).toBe(true); // find better (=more) criteria of recognizing Create Referral page
         });
             
         // describe('when user just navigates away', function() {
             // beforeEach(function() {
-                // browser.get('http://dev1.dentallinks.org/#/history');
+                // browser.get(baseUrl + '/#/history');
             // });
             
             // it('shows no warning and navigates away', function() {
                 // expect(browser.getCurrentUrl()).toContain('/history');
-                // browser.debugger();
             // });
         // });
         
-        // describe('when user changes patient, tries to leave and accepts losing of changes', function() {
-            
-            // beforeEach(function() {
-                // element(by.model('patient')).sendKeys('asdf');
-                // element(by.css('a.navbar-brand')).click();
-                // var alertDialog = browser.switchTo().alert();
-                // expect(alertDialog.getText()).toContain('unsaved');
-                // alertDialog.accept();
-            // });
-            
-            // it('navigates away', function() {
-                // expect(browser.getCurrentUrl()).toContain('/history');
-                // browser.debugger();
-            // });
-                
-        // });
     });
     
     afterEach(function() {
         // log out
         element(by.css('ul.nav.navbar-nav.navbar-right a.dropdown-toggle')).click();
         element(by.css('a[ng-click="logout()"]')).click();
+        expect(browser.getCurrentUrl()).toContain('/sign_in');
+        expect(element(by.buttonText('Login')).isPresent()).toBe(true);
     });
     
 });
