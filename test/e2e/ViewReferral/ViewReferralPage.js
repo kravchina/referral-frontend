81f3a432142a2309d2770a98b43f032af186f76c
anton-kaidalov <anton.kaydalov@gmail.com>
2015-01-18 18:46:10 -0500

    [#86373800] Test for attachment opening

--- test/e2e/ViewReferral/ViewReferralPage.js
+++ test/e2e/ViewReferral/ViewReferralPage.js
@@ -2,9 +2,7 @@ var ViewReferralPage = function() {
     this.url = "/#/view_referral";
     
     this.open = function() {
-        // TODO [ak] rework into opening the particular referral when we have 'expected' DB
-        // assuming we're on history page by default
-        element(by.repeater('referral in referrals').row(0)).click();
+        browser.get(this.url + '/215');
     };
     
 };
--- test/e2e/ViewReferral/ViewReferralSpec.js
+++ test/e2e/ViewReferral/ViewReferralSpec.js
@@ -16,6 +16,36 @@ var ViewReferralSpec = function() {
                 expect(element(by.css('button[ng-click="savePdf()"]')).isPresent()).toBe(true); // TODO [ak] consider adding more criteria of recognizing the page
             });
             
+            // test for #86373800
+            describe('when user clicks the attachment', function() {
+                
+                beforeEach(function() {
+                    element(by.repeater('attachment in referral.attachments').row(0)).click();
+                });
+                
+                it('opens a new tab with image in it', function(done) {
+                    browser.getAllWindowHandles().then(function(handles) {
+                        // assuming we now have exactly two tabs
+                        browser.switchTo().window(handles[1]).then(function() {
+                            expect(browser.driver.getCurrentUrl()).toContain('https://referral-server.herokuapp.com/attachment/?file=');
+                            browser.driver.isElementPresent(by.css('img')).then(function(present) {
+                                expect(present).toBeTruthy();
+                                browser.close();
+                                // switching back to first tab, otherwise EVERYTHING CRASHES!!11
+                                browser.switchTo().window(handles[0]).then(function() {
+                                    done(); // makes Jasmine wait for all these nested promises
+                                });
+                            });
+                        });
+                    });
+                });
+                
+            });
+            
+            it('shows patient copy button on the page', function() {
+                expect(element(by.css('button[ng-click="savePatientPdf()"]')).isPresent()).toBe(true);
+            });
+            
         });
     };
 };
