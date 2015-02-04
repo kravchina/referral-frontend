a61c820a864dccbd49ef2c6bd1b34c5f3f69cdae
Anton Mezerny <mezerny@gmail.com>
2015-02-04 03:39:18 +0200

    [#84423526] - Implement infinite scrolling for history page - fixed sequential elements appending

--- js/controllers/historyController.js
+++ js/controllers/historyController.js
@@ -3,30 +3,35 @@ var historyModule = angular.module('history', ['ui.bootstrap', 'infinite-scroll'
 historyModule.controller('HistoryController', ['$scope', 'Auth', 'Referral', 'Logger', function ($scope, Auth, Referral, Logger) {
 
 	$scope.limitTo = 20;
+    $scope.referrals = [];
 
     $scope.findReferralsByDateRange = function (start, end) {
     	$scope.start_date = start;
     	$scope.end_date = end;
-
+        $scope.busy = true;
         Referral.findByPractice({id: Auth.get().practice_id, start_date: start.toISOString(), end_date: end.toISOString(), term: $scope.query, limit: $scope.limitTo}, 
 			function(data){
 				Logger.log(data);
 				$scope.referrals = data.referrals;
 				$scope.referrals_total_count = data.referrals_total_count;
+                $scope.busy = false;
             });
     };
 
     $scope.findReferralsByTerm = function () {
     	Logger.log($scope.query);
-        Referral.findByPractice({id: Auth.get().practice_id, start_date: $scope.start_date.toISOString(), end_date: $scope.end_date.toISOString(), term: $scope.query, limit: $scope.limitTo}, 
+        $scope.busy = true;
+        Referral.findByPractice({id: Auth.get().practice_id, start_date: $scope.start_date.toISOString(), end_date: $scope.end_date.toISOString(), term: $scope.query, limit: $scope.limitTo},
 			function(data){
 				Logger.log(data);
 				$scope.referrals = data.referrals;
 				$scope.referrals_total_count = data.referrals_total_count;
+                $scope.busy = false;
 			});
     };
 
     $scope.addMoreItems = function(){
+        $scope.busy = true;
         var offset = $scope.referrals.length;
         Referral.findByPractice({id: Auth.get().practice_id, start_date: $scope.start_date.toISOString(), end_date: $scope.end_date.toISOString(), term: $scope.query, limit: $scope.limitTo, offset: offset},
             function(data){
@@ -34,6 +39,7 @@ historyModule.controller('HistoryController', ['$scope', 'Auth', 'Referral', 'Lo
                     $scope.referrals.push(data.referrals[i]);
                 }
                 $scope.referrals_total_count = data.referrals_total_count;
+                $scope.busy = false;
         });
     };
 
