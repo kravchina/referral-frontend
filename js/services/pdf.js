854438f5d6238982b731e8fce6c65fcbbc85def5
Slava Kravchina <slavakravchina@gmail.com>
2015-06-25 12:52:10 +0300

    [#86635814] - Notes on PDF do not show the user who added them

--- js/services/pdf.js
+++ js/services/pdf.js
@@ -151,12 +151,16 @@ dentalLinksPdf.factory('PDF', ['$filter', 'Spinner', 'ImageUtils', 'File', 'Auth
         if (notes && notes.length && notes.length > 0) {
             pdf.setFontType('normal');
             for (var k = 0; k < notes.length; k++) {
+                caret += fontSizeMm;
+                pdf.setFontType('bold');
+                pdf.text(pagePaddings.x, caret, notes[k].user.first_name + " " + notes[k].user.last_name + " " + moment(notes[k].created_at).format("MMM D, YYYY h:mm"));
+                pdf.setFontType('normal');
                 var noteLines = pdf.splitTextToSize(notes[k].message, fullSizeColWidth);
                 var noteHeight = noteLines.length * fontSizeMm;
                 caret = addPageIfNeeded(pdf, caret, noteHeight); // may include addHeader() resetting styles, but we have those default styles here anyway
                 caret += fontSizeMm;
                 pdf.text(pagePaddings.x, caret, noteLines); // assuming that note can fit at least a blank page. If not, what kind of "note" is it?..
-                caret += noteHeight - fontSizeMm;
+                caret += noteHeight;
             }
             caret += blocksPadding;
             caret = appendLineSeparator(pdf, caret);
