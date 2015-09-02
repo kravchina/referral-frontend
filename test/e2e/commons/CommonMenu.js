6ce9b61c67c9d07b7181b4d83290c46a338680d1
Anton K <anton.kaydalov@gmail.com>
2015-09-02 17:50:24 -0400

    Added a TODO.

--- test/e2e/commons/CommonMenu.js
+++ test/e2e/commons/CommonMenu.js
@@ -1,3 +1,5 @@
+// TODO [ak] this is also a site object. Should be moved closer to page objects. Functions like openMenu(), signOut() and possibly even expectMenuShown() should be relocated here
+
 var CommonMenu = function() {
     
     this.getTitleElement = function() {
