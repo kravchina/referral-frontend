87ebdaed9379f3de3bec0fae17d4692bbe6bb61f
Anton Mezerny <mezerny@gmail.com>
2015-09-03 20:35:24 +0300

    [#101587970] -Edit user dialog needs to be updated for users without login

---
+++ src/js/controllers/savePasswordsController.js
@@ -0,0 +1,18 @@
+angular.module('passwords')
+    .controller('SavePasswordController', ['$scope', '$state', '$stateParams', 'Alert', 'Auth', 'User', 'Notification', function ($scope, $state, $stateParams, Alert, Auth, User, Notification) {
+        $scope.alerts = [];
+        $scope.savePassword = function (model) {
+            model.add_password_token = $stateParams.add_password_token;
+            User.savePassword({user: model},
+                function (success) {
+                    Auth.set({token: success.authentication_token, email: success.email, roles: success.roles, is_admin: success.is_admin, id: success.id, practice_id: success.practice_id});
+                    Auth.current_user = success;
+                    Notification.success("Congratulations! You have successfully logged in with your new password");
+                    $state.go('history');
+                },
+                function (error) {
+                    Alert.error($scope.alerts, 'Error: can\'t change your password. Please try again later.');
+
+                });
+        };
+    }]);
