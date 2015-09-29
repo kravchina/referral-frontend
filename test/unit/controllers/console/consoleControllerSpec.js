b97bf5f4d3dd5d1baa31b08728a639a49ba4ae71
Slava Kravchina <slavakravchina@gmail.com>
2015-09-29 14:13:37 +0300

    [#102044288] - create Admin Console page and link it from navigation menu (dropdown)

--- test/unit/controllers/console/consoleControllerSpec.js
+++ test/unit/controllers/console/consoleControllerSpec.js
@@ -1,11 +1,6 @@
 describe("Testing Console Controller", function() {
     var consoleController;
-    var $scope = {
-        $on : function(){},
-        onChangeTab: function() {}
-
-    };
-    var $state;
+    var $scope, $state;
 
     beforeEach(function(){
         module('ui.router')
@@ -13,20 +8,50 @@ describe("Testing Console Controller", function() {
 
         inject(function($injector) {
             $state = $injector.get('$state');
+            $rootScope = $injector.get('$rootScope');
+            $scope = $rootScope.$new();
             consoleController = $injector.get('$controller')('ConsoleController', { $scope: $scope, $state: $state});
         });
     });
 
     it('should have a ConsoleController', function() {
         expect(consoleController).not.toBe(null);
-        console.log($scope.tabsData);
     });
 
-    it('should call StateChange', function() {
-        spyOn($scope, '$on');
-        console.log($scope);
-        $scope.$broadcast('$stateChangeSuccess'); // This is what I expect to trigger the `$on` method
-        expect($scope.$on).toHaveBeenCalled();;
+    it('check route is active', function() {
+        $state.is = function(state){
+            return state == 'active.route.name';
+        };
+
+        var isActive = $scope.active('active.route.name');
+
+        expect(isActive).toBe(true);
+    });
+
+    it('check route is inactive', function() {
+        $state.is = function(state){
+            return state == 'active.route.name';
+        };
+
+        var isActive = $scope.active('another.route.name');
+
+        expect(isActive).toBe(false);
+    });
+
+    it('check state change', function() {
+        $state.is = function(state){
+            return state == 'console.reports';
+        };
+        $rootScope.$broadcast('$stateChangeSuccess');
+        expect($scope.tabsData[1].active).toBe(true);
+    });
+
+    it('check onChangeTab function', function() {
+        $state.is = function(state){
+            return state == 'console.utilities';
+        };
+        $scope.onChangeTab();
+        expect($scope.tabsData[2].active).toBe(true);
     });
 
 });
