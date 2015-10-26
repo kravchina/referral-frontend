88bbac13158b4d8f95939617048d5133a4e42fa2
Slava Kravchina <slavakravchina@gmail.com>
2015-10-26 12:37:39 +0300

    [#105722314] - delete user action in UI is broken - test

--- test/e2e/specs/StandardLogin/Admin/AdminUsersSpec.js
+++ test/e2e/specs/StandardLogin/Admin/AdminUsersSpec.js
@@ -13,6 +13,15 @@ var AdminUsersSpec = function() {
             it('shows Users page', function() {
                 expect(element(by.css('th.cell-user')).isDisplayed()).toBe(true);
             });
+
+            it('check delete user action', function(){
+                element.all(by.css('[delete-button]')).each(function(item){
+                    item.click();
+                    expect(element.all(by.css('[delete-button].active')).count()).toBe(1);
+                });
+                element.all(by.css('[delete-button] .link-orange')).last().click();
+                expect(element.all(by.css('[delete-button].active')).count()).toBe(0);
+            });
             
         });
     };
