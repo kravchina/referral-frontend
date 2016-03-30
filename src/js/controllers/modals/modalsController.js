88e9240fa4c985cc9daafc94fecae283e0805445
Anton Mezerny <mezerny@gmail.com>
2016-03-30 18:06:12 +0300

    [#116177921] -  birthdate picker is too picky regarding the browser

--- src/js/controllers/modals/modalsController.js
+++ src/js/controllers/modals/modalsController.js
@@ -24,7 +24,7 @@ angular.module('modals')
         patient.practice_id = Auth.getOrRedirect().practice_id;
 
         function createPatient(){
-            Patient.save({patient: patient},
+            Patient.save({patient: {salutation: patient.salutation, first_name: patient.first_name, last_name: patient.last_name, middle_initial: patient.middle_initial, birthday: moment(patient.birthday).format('YYYY-MM-DD'), email: patient.email, phone: patient.phone, practice_id: patient.practice_id}},
                 function (success) {
                     ModalHandler.close($modalInstance, success);
                 },
@@ -88,10 +88,10 @@ angular.module('modals')
     $scope.datepickerStatus = {opened: false};
     $scope.alerts = [];
     $scope.salutations = ['Mr.', 'Ms.', 'Mrs.', 'Dr.'];
-    $scope.patient = {salutation: patientForEdit.salutation, first_name: patientForEdit.first_name, last_name: patientForEdit.last_name, middle_initial: patientForEdit.middle_initial, birthday: patientForEdit.birthday, email: patientForEdit.email, phone: patientForEdit.phone};//we need a copy of the object to be able to cancel changes (otherwise two-way binding changes the patient's data on parent page right away)
+    $scope.patient = {salutation: patientForEdit.salutation, first_name: patientForEdit.first_name, last_name: patientForEdit.last_name, middle_initial: patientForEdit.middle_initial, birthday: moment(patientForEdit.birthday).toDate(), email: patientForEdit.email, phone: patientForEdit.phone};//we need a copy of the object to be able to cancel changes (otherwise two-way binding changes the patient's data on parent page right away)
     $scope.ok = function (patient) {
         function updatePatient () {
-            Patient.update({id: patientForEdit.id}, {patient: patient},
+            Patient.update({id: patientForEdit.id}, {patient: {salutation: patient.salutation, first_name: patient.first_name, last_name: patient.last_name, middle_initial: patient.middle_initial, birthday: moment(patient.birthday).format('YYYY-MM-DD'), email: patient.email, phone: patient.phone}},
                 function (success) {
                     ModalHandler.close($modalInstance, success);
                 },
