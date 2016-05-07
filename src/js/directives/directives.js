d82aee1281433064797b05dc05d9809a7aac577a
Slava Kravchina <slavakravchina@gmail.com>
2016-05-07 12:43:54 +0300

    [#116421889] - loading indicator is showing from under transparent attachment images

--- src/js/directives/directives.js
+++ src/js/directives/directives.js
@@ -85,7 +85,11 @@ angular.module('dentalLinksDirectives')
                         element.attr( 'style',  "background: url(" + attributes.attachmentThumb + '&token=' + auth.token + '&from=' + auth.email + "&is_thumb=true) no-repeat top/contain, url('img/loader.gif') no-repeat top");
                         cssClass = '';
                     }else{
-                        element.attr( 'style',  "background: url(" + attributes.attachmentThumb + "&is_thumb=true) no-repeat top/contain, url('img/loader.gif') no-repeat top");
+                        element.attr( 'style',  "background: url('img/loader.gif') no-repeat top");
+                        angular.element('<img/>').attr('src', attributes.attachmentThumb + "&is_thumb=true").load(function() {
+                            angular.element(this).remove();
+                            element.attr('style', "background: url(" + attributes.attachmentThumb + "&is_thumb=true) no-repeat top/contain");
+                        });
                         cssClass = '';
                     }
                     break;
