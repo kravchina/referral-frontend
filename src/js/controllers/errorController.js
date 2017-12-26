d49e96478e9ca652b2f7906dc21dbef4961bb208
Anton Mezerny <mezerny@gmail.com>
2017-12-26 16:56:16 +0200

    [#152298851] - verifying a guest the second time - gives unreadable error message

--- src/js/controllers/errorController.js
+++ src/js/controllers/errorController.js
@@ -1,13 +1,21 @@
 angular.module('error')
-    .controller('ErrorController', ['$scope', '$stateParams', function($scope, $stateParams){
+    .controller('ErrorController', ['$scope', '$stateParams', 'localize', function($scope, $stateParams, localize){
 	$scope.error = {};
+	if(localize.resourceFileLoaded){
+        setupErrorMessage();
+    }else{
+        $scope.$on('localizeResourcesUpdated', function(){
+            setupErrorMessage();
+        });
+    }
 
-	if ($stateParams.error_key == '') {
-		$scope.error.header = 'default.error.header';
-		$scope.error.key = 'default.error';
-	} else {
-		$scope.error.header = $stateParams.error_key + '.header';
-		$scope.error.key = $stateParams.error_key;
-	}
-
+    function setupErrorMessage () {
+        if ($stateParams.error_key === '') {
+            $scope.error.header = 'default.error.header';
+            $scope.error.key = 'default.error';
+        } else {
+            $scope.error.header = $stateParams.error_key + '.header';
+            $scope.error.key = $stateParams.error_key;
+        }
+    }
 }]);
